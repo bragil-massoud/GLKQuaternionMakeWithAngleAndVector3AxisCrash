f8a972f5ea4014d17242abfe69576ea0a39ce65a
Simon Tietze <fqh786@motorola.com>
2013-05-16 12:14:59 -0700

    changed to demonstrate crash on GLKQuaternionMake

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    GLKQuaternionMakeWithAngleAndVector3Axis(0, GLKVector3Make(0,0,1))
-    true
+	#GLKQuaternionMakeWithAngleAndVector3Axis(0, GLKVector3Make(0,0,1))
+	GLKQuaternionMake(0.0,0.0,1.0,1.0)    
+	true
   end
 end
